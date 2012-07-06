c7aac15111cd76d5b201c5dea3792e931c52ff8d
Scott Montgomerie <montgomerie@shaw.ca>
2012-07-05 20:02:17 -0700

    Added the ability for custom variable tracking to Google Analytics

---
+++ lib/google-analytics/instance_methods.rb
@@ -0,0 +1,17 @@
+# This module holds all instance methods to be
+# included into ActionController::Base class
+# for enabling google analytics var tracking in a Rails app.
+#
+require "erb"
+
+module GoogleAnalytics
+  module InstanceMethods
+
+    def set_ga_custom_var(var)
+      raise "Must be instance of CustomVar" unless var.instance_of?(CustomVar)
+
+      # Store it in an instance var as well as the flash, in case of a redirect
+      self.env["google_analytics.custom_vars"] = var
+    end
+  end
+end
