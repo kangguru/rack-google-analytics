fb4ba4556832dba7201b7b05f9176fddb137f869
Scott Montgomerie <montgomerie@shaw.ca>
2012-07-09 19:38:13 -0700

    Added capability for event tracking

---
+++ lib/tracking/event.rb
@@ -0,0 +1,11 @@
+module GoogleAnalytics
+
+  # A Struct that mirrors the structure of a custom var defined in Google Analytics
+  # see https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
+  class Event < Struct.new(:category, :action, :label, :value, :noninteraction)
+
+    def write
+      ['_trackEvent', self.category, self.action, self.label,self.value, self.noninteraction].to_json
+    end
+  end
+end
