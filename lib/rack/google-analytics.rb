738e9afa837dcff7d56734fc4c06db60a14b8d57
Scott Montgomerie <montgomerie@shaw.ca>
2012-07-09 19:50:14 -0700

    Refactor

--- lib/rack/google-analytics.rb
+++ lib/rack/google-analytics.rb
@@ -4,6 +4,8 @@ require 'erb'
 module Rack
 
   class GoogleAnalytics
+
+    EVENT_TRACKING_KEY = "google_analytics.event_tracking"
     
     DEFAULT = { :async => true }
 
@@ -18,7 +20,20 @@ module Rack
       @status, @headers, @response = @app.call(env)
       return [@status, @headers, @response] unless html?
       response = Rack::Response.new([], @status, @headers)
-      @options[:tracker_vars] = env["google_analytics.custom_vars"]
+      @options[:tracker_vars] = env["google_analytics.custom_vars"] || []
+
+      if (@response.ok?)
+        # Write out the events now
+        @options[:tracker_vars] += (env[EVENT_TRACKING_KEY]) unless env[EVENT_TRACKING_KEY].blank?
+
+        # Get any stored events from a redirection
+        stored_events = env["rack.session"].delete(EVENT_TRACKING_KEY)
+        @options[:tracker_vars] += stored_events unless stored_events.blank?
+      elsif @response.redirection?
+        # Store the events until next time
+        env["rack.session"][EVENT_TRACKING_KEY] = env[EVENT_TRACKING_KEY]
+      end
+
       @response.each { |fragment| response.write inject(fragment) }
       response.finish
     end
