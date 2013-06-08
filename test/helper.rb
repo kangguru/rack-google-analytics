5188cebc77f3d231d9642061f108aefdeab1fb55
Scott Montgomerie <montgomerie@shaw.ca>
2013-06-07 23:46:44 -0700

    Removed "require 'rack/google-analytics'" from the test helper.rb. This was the reason we missed issue #16

--- test/helper.rb
+++ test/helper.rb
@@ -3,37 +3,42 @@ require 'test/unit'
 require 'shoulda'
 require 'rack/test'
 require 'active_support/core_ext/hash/slice'
-require File.expand_path('../../lib/rack/google-analytics',__FILE__)
-require File.expand_path('../../lib/tracking/custom_var',__FILE__)
-require File.expand_path('../../lib/tracking/event',__FILE__)
+require File.expand_path('../../lib/rack/google-analytics', __FILE__)
+require File.expand_path('../../lib/tracking/custom_var', __FILE__)
+require File.expand_path('../../lib/tracking/event', __FILE__)
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rack/google-analytics'
 
 class Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def app; Rack::Lint.new(@app); end
+  def app;
+    Rack::Lint.new(@app);
+  end
 
   def main_app(options)
-      lambda { |env|
+    lambda { |env|
 
-        env["google_analytics.event_tracking"] = options[:events] if options[:events]
-        env["google_analytics.custom_vars"] = options[:custom_vars] if options[:custom_vars]
+      env["google_analytics.event_tracking"] = options[:events] if options[:events]
+      env["google_analytics.custom_vars"] = options[:custom_vars] if options[:custom_vars]
 
-        request = Rack::Request.new(env)
-        case request.path
-        when '/' then [200,{ 'Content-Type' => 'application/html' },['<head>Hello world</head>']]
-        when '/test.xml' then [200,{'Content-Type' => 'application/xml'}, ['Xml here']]
-        when '/bob' then [200,{'Content-Type' => 'application/html'} ,['<body>bob here</body>']]
-        else [404,'Nothing here']
-        end
-      }
+      request = Rack::Request.new(env)
+      case request.path
+        when '/' then
+          [200, {'Content-Type' => 'application/html'}, ['<head>Hello world</head>']]
+        when '/test.xml' then
+          [200, {'Content-Type' => 'application/xml'}, ['Xml here']]
+        when '/bob' then
+          [200, {'Content-Type' => 'application/html'}, ['<body>bob here</body>']]
+        else
+          [404, 'Nothing here']
+      end
+    }
   end
-  
+
   def mock_app(options)
-    app_options = options.slice(:events, :custom_vars )
+    app_options = options.slice(:events, :custom_vars)
 
     builder = Rack::Builder.new
     builder.use Rack::GoogleAnalytics, options
